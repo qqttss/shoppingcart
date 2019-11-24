1a7a9e2e631648d276e4799f16528f738c96b7f5
qqttss <quantongshen@gmail.com>
2019-11-23 23:48:35 -0500

    Add discount

--- src/App.java
+++ src/App.java
@@ -5,16 +5,27 @@ import java.util.HashMap;
 public class App {
 
     public  static void main(String[] args) {
-        double sum = 0;
-        String[] items = {"Apple", "Apple", "Orange", "Apple"};
+        double sum1 = 0;
+        double sum2 = 0;
+        double sum3 = 0;
+
+        String[] items = {"Apple", "Apple", "Orange", "Orange", "Orange"};
 
         Checkout checkout = new Checkout();
 
         checkout.addProduct("Apple", 0.6);
         checkout.addProduct("Orange", 0.25);
 
-        sum = checkout.calculatePrice(items);
+        sum1 = checkout.calculatePrice(items);
 
-        System.out.println("Your total changes: \u00a3" + sum);
+        // Apply discount for Apple
+        sum2 = checkout.discount(items, "Apple", 2);
+
+        // Apply discount for Orange
+        sum3 = checkout.discount(items, "Orange", 3);
+
+        System.out.println("Your total charges: \u00a3" + sum1);
+        System.out.println("Your total charges with discount for Apple: \u00a3" + sum2);
+        System.out.println("Your total charges with discount for Apple and Orange: \u00a3" + sum3);
     }
 }
--- src/shoppingcart/Checkout.java
+++ src/shoppingcart/Checkout.java
@@ -5,7 +5,8 @@ import java.util.HashMap;
 public class Checkout {
 
     /** Map contains product prices with key being product name and value being price */
-    private HashMap<String, Double> basePrices;
+    private final HashMap<String, Double> basePrices;
+    private double totalPrice = 0;
 
     public Checkout() {
        basePrices = new HashMap<>();
@@ -23,7 +24,7 @@ public class Checkout {
     /**
      * Calculate the total price for the purchase list
      *
-     * @param listOfItems: the list of purchased items
+     * @param listOfItems the list of purchased items
      */
     public double calculatePrice(String[] listOfItems) {
         double sum = 0;
@@ -33,6 +34,29 @@ public class Checkout {
             sum += price;
         }
 
+        this.totalPrice = sum;
+        return sum;
+    }
+
+    /**
+     * Calculate discount for individual item
+     *
+     * @param listOfItems the list of purchased items
+     * @param product name of the discount product
+     * @param minAmount the minimum amount ro get the discount
+     * */
+    public double discount(String[] listOfItems, String product, int minAmount) {
+        double sum = 0d;
+        int count = 0;
+
+        for (String item : listOfItems) {
+            if (item.equals(product)) {
+                count++;
+            }
+        }
+
+        sum = this.totalPrice - count/minAmount * basePrices.getOrDefault(product, 0d);
+        this.totalPrice = sum;
         return sum;
     }
 }
