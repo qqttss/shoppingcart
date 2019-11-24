816c82d91e3c735876d99d989ca85c4ca7da8aec
qqttss <quantongshen@gmail.com>
2019-11-23 20:18:27 -0500

    Add javadoc for step 1

--- src/shoppingcart/Checkout.java
+++ src/shoppingcart/Checkout.java
@@ -3,16 +3,28 @@ package shoppingcart;
 import java.util.HashMap;
 
 public class Checkout {
+
+    /** Map contains product prices with key being product name and value being price */
     private HashMap<String, Double> basePrices;
 
     public Checkout() {
        basePrices = new HashMap<>();
     }
-
+    /**
+     * Add a new product and its price
+     *
+     * @param product: the product name
+     * @param price: the product price
+     * */
     public void addProduct(String product, double price) {
         this.basePrices.put(product, price);
     }
 
+    /**
+     * Calculate the total price for the purchase list
+     *
+     * @param listOfItems: the list of purchased items
+     */
     public double calculatePrice(String[] listOfItems) {
         double sum = 0;
 
